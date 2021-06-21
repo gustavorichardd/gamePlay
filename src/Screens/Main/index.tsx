160328100c711295ee320616622564af384012e4
Gustavo Richard <gustavo.richard@outlook.com.br>
2021-06-20 22:36:55 -0300

    Finalização da aula 01, criada a SignIn Screen e estilização, criado componente para Botão com ícone do Discord.

---
+++ src/Screens/Main/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles';
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text>MEIN PEIGI</Text>
+    </View>
+  );
+}
+
+export default Main;
